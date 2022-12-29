72782fd87dcc7209befaf42d0c827a8b4879151b
Nexisus <64307523+Nexisus@users.noreply.github.com>
2022-12-29 19:25:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -96,12 +96,12 @@ el.classList.add('active')
   const navBurger = document.getElementsByClassName('navbar-menuburger')[0]
   navBurger.addEventListener('click',()=>{
     if(burgerConuter%2==0){
-    navBurger.src='/img/cross.png'
+    navBurger.src='./img/cross.png'
     burgerConuter = burgerConuter +1
     navButtonsCont.style.display='flex'
     navbar.style.height='300px'
     }else{
-      navBurger.src='/img/menu-burger.png'
+      navBurger.src='./img/menu-burger.png'
       burgerConuter = burgerConuter +1
       navButtonsCont.style.display='none'
       navbar.style.height='100px'
